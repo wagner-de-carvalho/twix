1ca262ee48f5948319572e34557680cb6885aada
wagner <wagncarv@outlook.com>
2023-08-15 10:13:31 -0300

    Users actions create, update and get

---
+++ .iex.exs
@@ -0,0 +1,8 @@
+alias Twix.Repo
+alias Twix.Users.User
+
+user_params = %{
+  nickname: "Acme",
+  email: "acme@mail.com",
+  age: 30
+}
--- lib/twix.ex
+++ lib/twix.ex
@@ -1,9 +1,7 @@
 defmodule Twix do
-  @moduledoc """
-  Twix keeps the contexts that define your domain
-  and business logic.
+  alias Twix.Users
 
-  Contexts are also responsible for managing your data, regardless
-  if it comes from the database, an external API or others.
-  """
+  defdelegate create_user(params), to: Users.Create, as: :call
+  defdelegate get_user(id), to: Users.Get, as: :call
+  defdelegate update_user(params), to: Users.Update, as: :call
 end
