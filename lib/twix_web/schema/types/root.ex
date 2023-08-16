6bb15b98e913f8e45bc41b3fc1028d840251bb5c
wagner <wagncarv@outlook.com>
2023-08-16 00:01:45 -0300

    Catch errors, middlewares

--- lib/twix_web/schema/types/root.ex
+++ lib/twix_web/schema/types/root.ex
@@ -1,5 +1,6 @@
 defmodule TwixWeb.Schema.Types.Root do
   use Absinthe.Schema.Notation
+  alias Crudry.Middlewares.TranslateErrors
   alias TwixWeb.Resolvers.User, as: UserResolver
 
   import_types TwixWeb.Schema.Types.User
@@ -9,6 +10,7 @@ defmodule TwixWeb.Schema.Types.Root do
       arg :id, non_null(:id)
 
       resolve &UserResolver.get/2
+      middleware TranslateErrors
     end
   end
 
@@ -17,12 +19,14 @@ defmodule TwixWeb.Schema.Types.Root do
       arg :input, non_null(:create_user_input)
 
       resolve &UserResolver.create/2
+      middleware TranslateErrors
     end
 
     field :update_user, type: :user do
       arg :input, non_null(:update_user_input)
 
       resolve &UserResolver.update/2
+      middleware TranslateErrors
     end
   end
 end
