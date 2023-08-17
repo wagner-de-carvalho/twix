586cbcf09966415aa04d93e22901d9c8ced880ee
wagner <wagncarv@outlook.com>
2023-08-17 18:14:13 -0300

    Testing query graphql

---
+++ test/twix_web/schema_test.exs
@@ -0,0 +1,57 @@
+defmodule TwixWeb.SchemaTest do
+  use TwixWeb.ConnCase, async: true
+
+  describe "users query" do
+    test "returns a user", %{conn: conn} do
+      user_params = %{nickname: "acme test", age: 33, email: "acmetest@mail.com"}
+      {:ok, user} = Twix.create_user(user_params)
+
+      query = """
+        {
+          user(id: #{user.id}) {
+            email
+            nickname
+          }
+        }
+      """
+
+      response =
+        post(conn, "/api/graphql", %{query: query})
+        |> json_response(200)
+
+      expected_response = %{
+        "data" => %{"user" => %{"nickname" => "acme test", "email" => "acmetest@mail.com"}}
+      }
+
+      assert response == expected_response
+    end
+
+    test "when there is no user, returns an error", %{conn: conn} do
+      query = """
+        {
+          user(id: 999) {
+            email
+            nickname
+          }
+        }
+      """
+
+      response =
+        post(conn, "/api/graphql", %{query: query})
+        |> json_response(200)
+
+      expected_response = %{
+        "data" => %{"user" => nil},
+        "errors" => [
+          %{
+            "locations" => [%{"column" => 5, "line" => 2}],
+            "message" => "not_found",
+            "path" => ["user"]
+          }
+        ]
+      }
+
+      assert response == expected_response
+    end
+  end
+end
