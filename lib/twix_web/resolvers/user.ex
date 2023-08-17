75867b480b1a74c56a5b1751ea64bd93e894393c
wagner <wagncarv@outlook.com>
2023-08-17 14:15:23 -0300

    Using trigger to subscribe to topic

--- lib/twix_web/resolvers/user.ex
+++ lib/twix_web/resolvers/user.ex
@@ -1,22 +1,16 @@
 defmodule TwixWeb.Resolvers.User do
-  alias TwixWeb.Endpoint
-
-  @topic "new_follow_topic"
 
   def add_follower(%{input: %{user_id: user_id, follower_id: follower_id}}, _context) do
-    with {:ok, result} <- Twix.add_follower(user_id, follower_id) do
-      Absinthe.Subscription.publish(Endpoint, result, new_follow: @topic)
-      {:ok, result}
-    end
+    Twix.add_follower(user_id, follower_id)
   end
 
-  @spec create(
-          %{
-            :input => :invalid | %{optional(:__struct__) => none, optional(atom | binary) => any},
-            optional(any) => any
-          },
-          any
-        ) :: any
+  # def add_follower(%{input: %{user_id: user_id, follower_id: follower_id}}, _context) do
+  #  with {:ok, result} <- Twix.add_follower(user_id, follower_id) do
+  #   Absinthe.Subscription.publish(Endpoint, result, new_follow: @topic)
+  #  {:ok, result}
+  # end
+  # end
+
   def create(%{input: params}, _context), do: Twix.create_user(params)
   def get(%{id: id}, _context), do: Twix.get_user(id)
   def update(%{input: params}, _context), do: Twix.update_user(params)
--- lib/twix_web/schema/types/root.ex
+++ lib/twix_web/schema/types/root.ex
@@ -59,6 +59,8 @@ defmodule TwixWeb.Schema.Types.Root do
   object :root_subscription do
     field :new_follow, :add_follower_response do
       config fn _args, _context -> {:ok, topic: "new_follow_topic"} end
+
+      trigger :add_follower, topic: fn _context -> ["new_follow_topic"] end
     end
   end
 end
