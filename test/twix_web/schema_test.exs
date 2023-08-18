88346d9c88dce8d20201eba7dd8d04cf93ade896
wagner <wagncarv@outlook.com>
2023-08-17 22:30:33 -0300

    Testing mutations

--- test/twix_web/schema_test.exs
+++ test/twix_web/schema_test.exs
@@ -54,4 +54,27 @@ defmodule TwixWeb.SchemaTest do
       assert response == expected_response
     end
   end
+
+  describe "users mutation" do
+    test "when all params are valid, creates a user", %{conn: conn} do
+      query = """
+          mutation {
+          createUser(input: {age: 33, email: "acme@mail.com", nickname: "acme"}) {
+            email
+            nickname
+          }
+        }
+      """
+
+      response =
+        post(conn, "/api/graphql", %{query: query})
+        |> json_response(200)
+
+      expected_response = %{
+        "data" => %{"createUser" => %{"email" => "acme@mail.com", "nickname" => "acme"}}
+      }
+
+      assert response == expected_response
+    end
+  end
 end
