592642fa94def42f3e16f01c6d2f1222b82649f5
wagner <wagner.carvalho@outlook.com>
2024-02-12 18:30:04 -0300

    Create user mutation

--- lib/twix_web/resolvers/user.ex
+++ lib/twix_web/resolvers/user.ex
@@ -1,5 +1,6 @@
 defmodule TwixWeb.Resolvers.User do
   @moduledoc false
 
+  def create(%{input: params}, _context), do: Twix.create_user(params)
   def get(%{id: id}, _context), do: Twix.get_user(id)
 end
--- lib/twix_web/schema.ex
+++ lib/twix_web/schema.ex
@@ -7,4 +7,8 @@ defmodule TwixWeb.Schema do
   query do
     import_fields :root_query
   end
+
+  mutation do
+    import_fields :root_mutation
+  end
 end
