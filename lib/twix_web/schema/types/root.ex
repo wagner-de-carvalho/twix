efb496bbde8689f511a790568cf3a5a3b59167c5
wagner <wagncarv@outlook.com>
2024-02-12 23:58:50 -0300

    Removing absinthe error payload

--- lib/twix_web/schema/types/root.ex
+++ lib/twix_web/schema/types/root.ex
@@ -1,8 +1,7 @@
 defmodule TwixWeb.Schema.Types.Root do
   use Absinthe.Schema.Notation
-  import AbsintheErrorPayload.Payload
   import_types TwixWeb.Schema.Types.User
-  # alias Crudry.Middlewares.TranslateErrors
+  alias Crudry.Middlewares.TranslateErrors
   alias TwixWeb.Resolvers.User, as: UserResolver
 
   @doc """
@@ -19,19 +18,18 @@ defmodule TwixWeb.Schema.Types.Root do
   end
 
   object :root_mutation do
-    field :create_user, type: :user_payload do
+    field :create_user, type: :user do
       arg :input, non_null(:create_user_input)
 
       resolve &UserResolver.create/2
-      middleware &build_payload/2
-      # middleware TranslateErrors
+      middleware TranslateErrors
     end
 
     field :update_user, type: :user do
       arg :input, non_null(:update_user_input)
 
       resolve &UserResolver.update/2
-      # middleware TranslateErrors
+      middleware TranslateErrors
     end
   end
 end
--- lib/twix_web/schema/types/user.ex
+++ lib/twix_web/schema/types/user.ex
@@ -1,8 +1,5 @@
 defmodule TwixWeb.Schema.Types.User do
   use Absinthe.Schema.Notation
-  import AbsintheErrorPayload.Payload
-
-  import_types AbsintheErrorPayload.ValidationMessageTypes
 
   object :user do
     field :id, non_null(:id)
@@ -10,8 +7,6 @@ defmodule TwixWeb.Schema.Types.User do
     field :age, non_null(:integer)
     field :email, non_null(:string)
   end
-
-  payload_object(:user_payload, :user)
 
   input_object :create_user_input do
     field :nickname, non_null(:string)
