3c1752b065612bf7ba8eb10a893f0acc965ec413
wagner <wagncarv@outlook.com>
2024-02-13 00:33:45 -0300

    Aadding field description

--- lib/twix_web/schema/types/post.ex
+++ lib/twix_web/schema/types/post.ex
@@ -2,14 +2,16 @@ defmodule TwixWeb.Schema.Types.Post do
   @moduledoc false
   use Absinthe.Schema.Notation
 
+  @desc "Post logic representation"
   object :post do
-    field :id, non_null(:id)
-    field :text, non_null(:string)
-    field :likes, non_null(:integer)
+    field :id, non_null(:id), description: "Post ID"
+    field :text, non_null(:string), description: "A valid text"
+    field :likes, non_null(:integer), description: "A valid like number"
   end
 
+  @desc "Required params to create a new post"
   input_object :create_post_input do
-    field :user_id, non_null(:id)
-    field :text, non_null(:string)
+    field :user_id, non_null(:id), description: "User ID"
+    field :text, non_null(:string), description: "A valid text"
   end
 end
--- lib/twix_web/schema/types/user.ex
+++ lib/twix_web/schema/types/user.ex
@@ -1,24 +1,27 @@
 defmodule TwixWeb.Schema.Types.User do
   use Absinthe.Schema.Notation
 
+  @desc "User logic representation"
   object :user do
-    field :id, non_null(:id)
-    field :nickname, non_null(:string)
-    field :age, non_null(:integer)
-    field :email, non_null(:string)
-    field :posts, list_of(:post)
+    field :id, non_null(:id), description: "User ID"
+    field :nickname, non_null(:string), description: "A valid nickname"
+    field :age, non_null(:integer), description: "Must be greater than or equal to 18"
+    field :email, non_null(:string), description: "Valid e-mail"
+    field :posts, list_of(:post), description: "A list of posts"
   end
 
+  @desc "Required params to create a new user"
   input_object :create_user_input do
-    field :nickname, non_null(:string)
-    field :age, non_null(:integer)
-    field :email, non_null(:string)
+    field :nickname, non_null(:string), description: "A valid nickname"
+    field :age, non_null(:integer), description: "Must be greater than or equal to 18"
+    field :email, non_null(:string), description: "Valid e-mail"
   end
 
+  @desc "Required params to update an user"
   input_object :update_user_input do
-    field :id, non_null(:id)
-    field :nickname, :string
-    field :age, :integer
-    field :email, :string
+    field :id, non_null(:id), description: "User ID"
+    field :nickname, :string, description: "A valid nickname"
+    field :age, :integer, description: "Must be greater than or equal to 18"
+    field :email, :string, description: "Valid e-mail"
   end
 end
